48ce497751670bcf7d52ee8af38a917952f6c0d8
mmerle <35971019+mmerle@users.noreply.github.com>
2021-05-13 00:07:19 -0400

    fix yeelight file names

--- raycast/script-commands/home/yeelight-set-brightness.sh
+++ raycast/script-commands/home/yeelight-set-brightness.sh
@@ -2,11 +2,13 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Toggle Light Bar
+# @raycast.title Set Light Bar Brightness
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.packageName Home
 # @raycast.icon images/yeelight.png
 
-yeecli toggle
+# @raycast.argument1 { "type": "text", "placeholder": "100" }
+
+yeecli brightness $1
--- raycast/script-commands/home/yeelight-toggle.sh
+++ raycast/script-commands/home/yeelight-toggle.sh
@@ -2,13 +2,11 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Set Light Bar Brightness
+# @raycast.title Toggle Light Bar
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.packageName Home
 # @raycast.icon images/yeelight.png
 
-# @raycast.argument1 { "type": "text", "placeholder": "100" }
-
-yeecli brightness $1
+yeecli toggle
