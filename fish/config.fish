089d86685d3b32685aff3299f22159ef1ea08fc5
mmerle <35971019+mmerle@users.noreply.github.com>
2021-07-27 20:21:33 -0400

    zsh to fish + cleanup

---
+++ fish/config.fish
@@ -0,0 +1,23 @@
+set fish_greeting
+
+# aliases
+alias la="ls -A"
+alias vim="nvim"
+alias rm="rm -i"
+alias yt-dl="youtube-dl -f 140"
+
+# git aliases
+alias g="git"
+alias gs="git status"
+alias ga='git add'
+alias gc='git commit -m'
+alias go='git checkout'
+alias gu='git push'
+alias gp='git pull'
+alias gb='git branch'
+alias gm='git merge'
+alias gl='git log -n 10 --graph --decorate --oneline --no-merges'
+
+# config aliases
+alias ,fish='code ~/.config/fish/config.fish'
+alias ,kitty='code ~/.config/kitty/kitty.conf'
