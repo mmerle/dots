d48c1fdfc64553d0060f05578c3cec6e30b9825a
mmerle <mattdommerle@gmail.com>
2021-08-16 19:43:53 -0400

    change bind mode symbols

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,5 +1,20 @@
 function fish_right_prompt
-	echo (fish_git_prompt) [$fish_bind_mode] 
+  set -g __fish_git_prompt_color $subtle
+	echo (fish_git_prompt)
+  switch $fish_bind_mode
+    case default
+      set_color $subtle
+      echo -n ' [N]'
+    case insert
+      set_color $subtle
+      echo -n ' [I]'
+    case replace_one
+      set_color $subtle
+      echo -n ' [R]'
+    case visual
+      set_color $subtle
+      echo -n ' [V]'
+  end
 end
 
 function fish_mode_prompt
