0a6b98d0c9d824d1239a16a739af98e00127c624
mmerle <mattdommerle@gmail.com>
2021-10-05 21:18:24 -0400

    update fish right prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,21 +1,27 @@
 function fish_right_prompt
-  set -g __fish_git_prompt_color $subtle
-	echo (fish_git_prompt)
-  switch $fish_bind_mode
-    case default
-      set_color $subtle
-      echo -n ' [N]'
-    case insert
-      set_color $subtle
-      echo -n ' [I]'
-    case replace_one
-      set_color $subtle
-      echo -n ' [R]'
-    case visual
-      set_color $subtle
-      echo -n ' [V]'
-  end
+  set -g __fish_git_prompt_showupstream verbose
+  set -g __fish_git_prompt_char_upstream_ahead ' ↑'
+  set -g __fish_git_prompt_char_upstream_behind ' ↓'
+  set -g __fish_git_prompt_char_upstream_diverged ' ↕'
+  set -g __fish_git_prompt_char_upstream_equal
+  set -g __fish_git_prompt_char_upstream_prefix
+  printf '%s %s' (set_color yellow; fish_git_prompt; set_color normal) (mode_indicator)
 end
 
+function mode_indicator
+  switch $fish_bind_mode
+    case default
+      echo '[N]'
+    case insert
+      echo '[I]'
+    case visual
+      echo '[V]'
+    case replace_one
+      echo '[R]'
+  end
+  set_color normal
+end
+
+# disable default vi mode indicator
 function fish_mode_prompt
 end
