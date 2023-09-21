5e599700fe604088aa4ec3b5a9bb4289d419001b
mmerle <35971019+mmerle@users.noreply.github.com>
2023-09-20 23:57:37 -0400

    exa -> eza

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,15 @@ set -gx PATH "$PNPM_HOME" $PATH
 
 set -gx EDITOR nvim
 
+# disable analytics of commonly used tools
+set HOMEBREW_NO_ANALYTICS 1
+set NEXT_TELEMETRY_DISABLED 1
+
+# pfetch
+set PF_INFO "title os host uptime pkgs shell editor wm de"
+
 fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
 fish_add_path $HOME/.cargo/bin
 
 # enable vi mode
@@ -39,9 +47,9 @@ function fish_mode_prompt
 end
 
 # aliases
-alias ls='exa'
-alias la='exa -la'
-alias lt='exa -T --git-ignore'
+alias ls='eza'
+alias la='eza -la'
+alias lt='eza -T --git-ignore'
 alias vim='nvim'
 alias rm='trash'
 alias reload='exec $SHELL -l'
