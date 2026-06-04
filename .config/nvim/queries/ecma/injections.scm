8503cab4b8e468d403101610c08e220edc276566
mmerle <35971019+mmerle@users.noreply.github.com>
2026-06-04 17:38:51 -0400

    add regular tagged templates

--- .config/nvim/queries/ecma/injections.scm
+++ .config/nvim/queries/ecma/injections.scm
@@ -4,21 +4,33 @@
 ([
   (variable_declarator
     (comment) @injection.language
-    value: (template_string) @injection.content)
+    value: (template_string
+      (string_fragment) @injection.content))
 
   (assignment_expression
     (comment) @injection.language
-    right: (template_string) @injection.content)
+    right: (template_string
+      (string_fragment) @injection.content))
 
   (arguments
     (comment) @injection.language
-    (template_string) @injection.content)
+    (template_string
+      (string_fragment) @injection.content))
 
   (return_statement
     (comment) @injection.language
-    (template_string) @injection.content)
+    (template_string
+      (string_fragment) @injection.content))
 ]
   (#lua-match? @injection.language "^/%*%s*[%w_-]+%s*%*/$")
   (#gsub! @injection.language "^/%*%s*([%w_-]+)%s*%*/$" "%1")
-  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.combined)
+  (#set! injection.include-children))
+
+; tagged template literals:
+(call_expression
+  function: (identifier) @injection.language
+  arguments: (template_string
+    (string_fragment) @injection.content)
+  (#set! injection.combined)
   (#set! injection.include-children))
