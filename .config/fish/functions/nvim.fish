33710998de4b303b899b87df6632065e006c162a
mmerle <35971019+mmerle@users.noreply.github.com>
2026-07-20 19:11:22 -0400

    fix tmux/nvim auto colorscheme

--- .config/fish/functions/nvim.fish
+++
@@ -1,7 +0,0 @@
-function nvim -w nvim -d "Start Neovim with adaptive background"
-    if test "$DARK_MODE" = on
-        command nvim -c 'lcd %:p:h' -c 'set background=dark' $argv
-    else
-        command nvim -c 'lcd %:p:h' -c 'set background=light' $argv
-    end
-end
